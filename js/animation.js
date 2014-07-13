029ed05960573a3c0a5fc2bd0024027ca8c25874
vandosant <sjskender@gmail.com>
2014-07-12 18:09:28 -0600

    First attempt

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,30 @@
 window.Animation = {
 
-  initialize: function(divs){
+  initialize: function (divs) {
+    var tailIndex = 0;
+    var bodyIndex = 1;
+    var headIndex = 2;
+    divs[tailIndex].css({background: 'black'});
+    divs[bodyIndex].css({background: 'black'});
+    divs[headIndex].css({background: 'black'});
 
-    // divs is an array of the divs on the page
-    // you can change the background color of the divs, like so:
+    setInterval(function () {
+      divs[tailIndex].css({background: 'white'});
 
-    divs[0].css({background: 'black'});
-
+      if (headIndex === divs.length-1){
+        headIndex = 0;
+      } else if (bodyIndex === divs.length-1) {
+        bodyIndex = 0;
+      } else if (tailIndex === divs.length-1) {
+        tailIndex = 0;
+      } else {
+        tailIndex++;
+        bodyIndex++;
+        headIndex++;
+      }
+      divs[tailIndex].css({background: 'black'});
+      divs[bodyIndex].css({background: 'black'});
+      divs[headIndex].css({background: 'black'});
+    }, 80)
   }
-
 };
