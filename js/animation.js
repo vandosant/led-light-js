f340308fe7d2722a1ba0d9a9818906c3baa6f2ec
vandosant <sjskender@gmail.com>
2014-07-12 18:12:44 -0600

    Fixed stagger at index reset

--- js/animation.js
+++ js/animation.js
@@ -13,10 +13,16 @@ window.Animation = {
 
       if (headIndex === divs.length-1){
         headIndex = 0;
+        bodyIndex++;
+        tailIndex++;
       } else if (bodyIndex === divs.length-1) {
         bodyIndex = 0;
+        headIndex++;
+        tailIndex++;
       } else if (tailIndex === divs.length-1) {
         tailIndex = 0;
+        bodyIndex++;
+        headIndex++;
       } else {
         tailIndex++;
         bodyIndex++;
@@ -25,6 +31,6 @@ window.Animation = {
       divs[tailIndex].css({background: 'black'});
       divs[bodyIndex].css({background: 'black'});
       divs[headIndex].css({background: 'black'});
-    }, 80)
+    }, 60)
   }
 };
